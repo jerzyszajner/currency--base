817ba4ae014352358aaf2200979cc71046af072c
jerzy szajner <jerzy.szajner@gmail.com>
2024-01-19 18:55:48 +0100

    feat: enhance convertPLNToUSD with robust testing
    Add unit tests for the convertPLNToUSD function covering five specific cases:
    1. Verifying correct currency conversion for valid numerical inputs.
    2. Ensuring NaN is returned for string inputs to prevent invalid conversions.
    3. Validating NaN return for undefined inputs, addressing cases of missing arguments.
    4. Testing for 'Error' return on non-string, non-numeric inputs to handle invalid data types.
    5. Confirming the return of '$0.00' for negative number inputs, as per business logic.
    
    These tests ensure comprehensive validation of the convertPLNToUSD function, enhancing its reliability and correctness in various scenarios.

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,7 +1,27 @@
 export const convertPLNToUSD = (PLN) => {
 
+  // Check if the input is a string and return NaN in such case
+  if (typeof PLN === 'string') {
+    return NaN;
+  }
+
+  // Check if the input argument is not provided (undefined) and return NaN
+  if (PLN === undefined) {
+    return NaN;
+  }
+
+  // Check if the input is different than number and string and return 'Error'
+  if (typeof PLN !== 'number') {
+    return 'Error';
+  }
+
+  // Check if the input is lower than zero and return '$0.00'
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
   const PLNtoUSD = PLN / 3.5;
-  
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
